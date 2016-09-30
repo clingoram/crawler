61db5f9d7d8be541f63c088c0b104ee3972ea5ea
clingoram <llingchen.27@gmail.com>
2016-09-30 14:44:55 +0800

    Update and rename python to crawler.py

---
+++ crawler.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import requests
+import re
+from bs4 import BeautifulSoup
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def getNews():
+    url = 'https://news.google.com/?sdm=FADEOUT&authuser=0'
+    req = requests.get(url, verify=False)
+    soup = BeautifulSoup(req.text,'html.parser')
+
+    for link in soup.find_all('a', {'class':'article'}): 
+        href = 'https://news.google.com/'+ link.get('href')  
+        title = link.string
+        print(href)
+        print(title)
+
+getNews()
+
